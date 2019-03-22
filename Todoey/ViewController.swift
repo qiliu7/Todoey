a05bd880076ac2e5bfff7157e5540296f7306e65
Qi Liu <qil244@mail.usask.ca>
2019-03-22 17:02:04 +0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Kappa on 2019/3/22.
+//  Copyright © 2019 Qi Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
